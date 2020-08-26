98265a8683001e16139af19faf2034cecbc05b50
Alikhan219 <merabhai786786@gmail.com>
2020-08-26 12:10:24 +0500

    i have no need of random shapes so i deleted the random shapes stuff

--- game.js
+++ game.js
@@ -34,19 +34,6 @@ Bodies.rectangle(800,300,20,600,{
 })
 ]
 World.add(world,shape)
-for(let i=0; i<50; i++){
-if(Math.random() > 0.5){
-    World.add(world, Bodies.rectangle(Math.random() *width, Math.random() * height, 50,50, {
-        render :{
-            fillStyle: 'blue'
-        }
-    }))
-}else{
-World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-    render :{
- fillStyle: 'blue'
-    }
-}));
-}
-   
-}
+
+
+
